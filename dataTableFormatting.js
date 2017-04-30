9ab7aef4ef72ea0c85603514399a43c630f26254
Will C <w.chou06@gmail.com>
2017-04-30 17:02:58 -0400

    Update dataTableFormatting.js

--- dataTableFormatting.js
+++ dataTableFormatting.js
@@ -1,3 +1,18 @@
+/*
+Modified work from this source:http://spotfireplanet.com/style-cross-table-and-data-table-with-javascript/
+This script modifies the default styles for the built in classes.
+
+The below classes are associated with DataTables and CrossTables
+sfpc-row-header
+sfpc-column-header
+sf-element-table-cell.sfpc-section-content
+sf-element-table-cell.sfpc-total-value
+sf-element-table-cell
+
+To use the script, include it in a text field. It will run when the report loads.
+
+If there are any syntax errors, it will cause all dashboard views that are listed after it to be greyed out and the text field to be inaccessable.
+*/
 if($('#my-style').length){
    // do nothing style sheet already exists
 } else {
