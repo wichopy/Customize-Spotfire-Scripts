1db97ca680dde3a4f57796a6093c06227d97cf06
Will C <w.chou06@gmail.com>
2017-04-30 17:05:39 -0400

    Update dataTableFormatting.js

--- dataTableFormatting.js
+++ dataTableFormatting.js
@@ -17,13 +17,15 @@ if($('#my-style').length){
    // do nothing style sheet already exists
 } else {
    // add CSS to the body
-   $('body').append($('<style id="my-style">\
+   $('body').append($('\
+	<style id="my-style">\
          .sfpc-row-header {background-color:white;vertical-align: center}\
          .sfpc-column-header {background-color:lightgrey;vertical-align: center}\
-		 .sf-element-table-cell.sfpc-section-content{color: green;text-align: center !important;vertical-align: center}\
+	 .sf-element-table-cell.sfpc-section-content{color: green;text-align: center !important;vertical-align: center}\
          .sf-element-table-cell.sfpc-total-value {background-color:white;text-align:center;vertical-align: center}\
-		 .sf-element-table-cell{color: black !important;text-align: center !important;vertical-align: center}\
+	 .sf-element-table-cell{color: black !important;text-align: center !important;vertical-align: center}\
         </style>', {
-        id: 'my-style' 
-   }));
+          id: 'my-style' 
+   	}
+   ));
 }
